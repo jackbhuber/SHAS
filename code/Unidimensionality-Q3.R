b2385e8453b33bdf1dddf93b9d2ef28cd5792a2d
jackbhuber <jack.bernard.huber@gmail.com>
2022-04-30 08:35:07 -0700

    Dialed in the Q3 statistic

--- code/Unidimensionality-Q3.R
+++ code/Unidimensionality-Q3.R
@@ -1,22 +1,13 @@
 # Q3 STATISTIC
 library(sirt)
-items <- read.csv("data/ditems_l.csv")
-
-# 1 - ESIMATE THE RASCH MODEL
-mod <- sirt::rasch.mml2(items)
-mod$item
-
-# 2 - ESTIMATE WLEs
-mod.wle <- sirt::wle.rasch( dat=items, b=mod$item$b )
-
-# 3 - CALCULATE Q3
-mod.q3 <- sirt::Q3( dat=items, theta=mod.wle$theta, b=mod$item$b )
-mod.q3
-
+items <- read.csv("data/ditems_l.csv") # grab the data
+mod <- sirt::rasch.mml2(items) # estimate the Rasch model
+mod.wle <- sirt::wle.rasch( dat=items, b=mod$item$b) # estimate WLEs
+mod.q3 <- sirt::Q3( dat=items, theta=mod.wle$theta, b=mod$item$b ) # estimate Q3
 
 # plot Q3 statistics
-I <- ncol(data.read)
+I <- ncol(items)
 image( 1:I, 1:I, mod.q3$q3.matrix, col=gray( 1 - (0:32)/32),
        xlab="Item", ylab="Item")
-abline(v=c(5,9)) # borders for testlets
-abline(h=c(5,9))
+abline(v=c(53,66)) # borders for testlets
+abline(h=c(53,66))
