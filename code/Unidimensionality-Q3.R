c6c3c4ba17bac80fee491e86470266b7436e0511
jackbhuber <jack.bernard.huber@gmail.com>
2022-04-30 21:45:25 -0700

    experimenting with child documents, clearing out environment at document end

--- code/Unidimensionality-Q3.R
+++ code/Unidimensionality-Q3.R
@@ -2,8 +2,8 @@
 library(sirt)
 items <- read.csv("data/ditems_l.csv") # grab the data
 mod <- sirt::rasch.mml2(items) # estimate the Rasch model
-mod.wle <- sirt::wle.rasch( dat=items, b=mod$item$b) # estimate WLEs
-mod.q3 <- sirt::Q3( dat=items, theta=mod.wle$theta, b=mod$item$b ) # estimate Q3
+mod.wle <- sirt::wle.rasch(dat=items, b=mod$item$b) # estimate WLEs
+mod.q3 <- sirt::Q3(dat=items, theta=mod.wle$theta, b=mod$item$b ) # estimate Q3
 
 # plot Q3 statistics
 I <- ncol(items)
