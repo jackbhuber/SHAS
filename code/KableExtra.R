b7a076c18c2566ec005d026c041e5e78c376e228
jackbhuber <jack.bernard.huber@gmail.com>
2022-05-08 08:27:03 -0700

    still trying to dial in conditional formatting

--- code/KableExtra.R
+++ code/KableExtra.R
@@ -1,5 +1,11 @@
 # KABLE EXTRA TABLES
+# this works
 library(kableExtra)
-items.tbl %>%
-  kable(digits = 3, align = "ccccc", caption = 'Graded Response Model Item Statistics') %>%
-  kable_styling(bootstrap_options = "condensed", font_size = 14, fixed_thead = T)
+library(dplyr)
+round(items.tbl,2) %>%
+  mutate(
+    outfit = cell_spec(outfit, color = ifelse(outfit > .5 & outfit < 1.5 ,"black",c("red",bold=TRUE))),
+    infit = cell_spec(infit, color = ifelse(infit > .5 & infit < 1.5 , "black","red"))
+  ) %>%
+  kbl(escape = F, align = "rrrrrrrrr", caption = 'Rating Scale Model Item Parameters and Fit Statistics') %>%
+  kable_styling("condensed", font_size = 12, fixed_thead = T, full_width = TRUE)
